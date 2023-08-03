8ab06ce3fb1a3690c1970c19c8ca65a58cb8b0c4
FranNinaa <francinesantoss85@gmail.com>
2023-08-02 21:24:45 -0300

    aula de quarta

---
+++ Models/Endereco.cs
@@ -0,0 +1,16 @@
+﻿namespace JPweb.Models
+{
+    public class Endereco
+    {
+        public string Logradouro { get; set; }
+
+        public string Complemento { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Localidade { get; set; }
+
+        public string Uf{ get; set; }
+
+    }
+}
